5dc55fdd7e6a046da88d89e625a5903c842a7472
Shamil Khannanov <jk62nona@studserv.uni-leipzig.de>
2022-05-23 11:58:08 +0200

    fixed ID datatype

--- src/main/java/parser/XmlParser.java
+++ src/main/java/parser/XmlParser.java
@@ -52,4 +52,58 @@ public class XmlParser implements Reader{
         return null;
     }
 
+    public void parseCategories(NodeList list, SessionFactory sessionFactory) {
+
+        for (int i = 0; i < list.getLength(); i++) {
+            if (returnTagOfNode(list.item(i)).equals("category")) {
+
+                GenericDao<CategoryEntity> daoCat = new GenericDao<>(sessionFactory);
+                CategoryEntity entityCat = new CategoryEntity();
+
+                entityCat.setCategoryName(returnTextValueOfNode(list.item(i)));
+                daoCat.create(entityCat);
+                categoryDaoMap.put(returnTextValueOfNode(list.item(i)), entityCat.getCategoryId());
+
+                if (returnTagOfNode(list.item(i).getParentNode()).equals("category")){
+                    entityCat.setSuperCategory(categoryDaoMap.get(returnTextValueOfNode(list.item(i))));
+                }
+                parseCategories(list.item(i).getChildNodes(), sessionFactory);
+
+            }
+            else if ((returnTagOfNode(list.item(i))).equals("item")){
+                GenericDao<ProductCategoryEntity> productDao = new GenericDao(sessionFactory);
+                ProductCategoryEntity entityProduct = new ProductCategoryEntity();
+                productDao.create(entityProduct);
+                entityProduct.setProdId(list.item(i).getFirstChild().getNodeValue());
+
+                entityProduct.setCategoryId(categoryDaoMap.get((returnTextValueOfNode(list.item(i).getParentNode()))));
+            }
+        }
+
+    }
+
+    String returnTextValueOfNode(Node node){
+        String value;
+        if (node == null){
+            value = "none";
+            return value;
+        }
+        else if (node.getFirstChild() == null){
+            value = "none";
+            return value;
+        }
+        else value = node.getFirstChild().getNodeValue().trim();
+        return value;
+    }
+
+    String returnTagOfNode(Node node){
+        String value;
+        if (node == null){
+            value = "";
+            return  value;
+        }
+        else value = node.getNodeName().trim();
+        return value;
+    }
+
 }
