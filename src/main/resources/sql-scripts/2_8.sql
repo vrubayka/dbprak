499a02ee916e448b4a8cdc2639d881345fc64727
Shamil Khannanov <jk62nona@studserv.uni-leipzig.de>
2022-06-30 21:51:26 +0200

    Queries done, result sets added

--- src/main/resources/sql-scripts/2_8.sql
+++ src/main/resources/sql-scripts/2_8.sql
@@ -1,3 +1,3 @@
 SELECT DISTINCT person_name from person p natural join author a natural join dvd_person d natural join cd_artist cd
-WHERE p.person_id = a.person_id AND p.person_id = d.person_id AND cd.artist_id = p.person_id
+WHERE p.person_id = a.person_id AND p.person_id = d.person_id OR cd.artist_id = p.person_id
 ORDER BY person_name;
