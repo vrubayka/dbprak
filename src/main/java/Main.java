7dd76e32813c02338f87ff7caf96d95d70eafa12
Shamil Khannanov <jk62nona@studserv.uni-leipzig.de>
2022-09-12 14:06:52 +0200

    getTopProducts done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,7 @@
-import entities.*;
 import frontend.Menu;
-import logging.ReadLog;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.query.MutationQuery;
-import parser.CSVParser;
-import parser.CategoryReader;
-import parser.XmlParser;
-import queries.HibernateQueries;
-
-import java.io.File;
 import java.io.IOException;
-import java.lang.annotation.Annotation;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class Main {
 
