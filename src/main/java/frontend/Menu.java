e4c4a36f56dad11f051e43d15de189257d8c008a
Shamil Khannanov <jk62nona@studserv.uni-leipzig.de>
2022-09-13 11:43:29 +0200

    addNewReview started

--- src/main/java/frontend/Menu.java
+++ src/main/java/frontend/Menu.java
@@ -1,13 +1,16 @@
 package frontend;
 
 import entities.ProductEntity;
+import entities.ReviewEntity;
 import middle.MenuMapper;
 import middle.wrapperClass.CategoryNode;
 import middle.wrapperClass.User;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.List;
-import java.util.Scanner;
+import javax.print.attribute.standard.DateTimeAtProcessing;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.*;
 
 public class Menu {
 
@@ -160,7 +163,52 @@ public class Menu {
 
     private void option8() { //addNewReview
         System.out.println("Option addNewReview ausgewaehlt");
-        System.out.println("Gib ");
+        System.out.println("1 - existierenden Review zeigen\n2 - neues Review schreiben");
+        String auswahl = scanner.nextLine();
+        String output;
+        if (auswahl.equals("1")){
+            System.out.println("Geben Sie den Produkt-ID ein");
+            String prodId = scanner.nextLine();
+            System.out.println("Geben Sie den Username ein");
+            String username = scanner.nextLine();
+            ReviewEntity review = new ReviewEntity();
+            review.setUsername(username);
+            review.setProdId(prodId);
+            //output = mapper.addNewReview(review, false);
+        }
+        else if (auswahl.equals("2")){
+            System.out.println("Geben Sie den Produkt-ID ein");
+            String prodId = scanner.nextLine();
+
+            System.out.println("Geben Sie den Username ein");
+            String username = scanner.nextLine();
+
+            System.out.println("Geben Sie den Rating ein");
+            int rating = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("Geben Sie die Zusammenfassung");
+            String summary = scanner.nextLine();
+
+            java.sql.Date date = null;
+            date = (java.sql.Date.valueOf(LocalDate.now()));
+
+
+            System.out.println("Geben Sie den Review ein");
+            String reviewText = scanner.nextLine();
+
+
+            ReviewEntity review = new ReviewEntity();
+            review.setHelpfulRating(0);
+            review.setReviewText(reviewText);
+            review.setProdId(prodId);
+            review.setRating(rating);
+            review.setReviewdate(date);
+            review.setUsername(username);
+            review.setReviewSum(summary);
+            mapper.addNewReview(review, );
+
+        }
     }
 
     private void option9() { //getTrolls
